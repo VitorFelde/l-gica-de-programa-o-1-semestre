ef3eb2f343afac3fa04fe5a7704d2808f2b4c617
VitorFelde <feldevitor@gmail.com>
2026-04-24 10:35:58 -0300

    finalizado

--- lista 7/atividade7.c
+++ lista 7/atividade7.c
@@ -6,7 +6,7 @@ quantidade de torcedores do Coritiba e a quantidade de torcedores do Foz. Ao fin
 porcentagem de torcedores de cada time em relação ao total.*/
 
 #include <stdio.h>
-#include <ctype.h>
+#include <ctype.h> /*para usar o toupper*/
 
 int main () {
     
@@ -15,18 +15,57 @@ int main () {
     int total, atle = 0, foz = 0, coxa = 0; 
     
     printf ("Digite A para Atlético - Digite C para Coritiba - Digite F para Foz - Digite S para sair do programa");
+    /*deixando mais claro para o usuario o que ele deve digitar*/
     
     
     
     do {
-        printf ("Para qual time você torce? [A][C][F][S]?: ");
+        printf ("\nPara qual time você torce? [A][C][F][S]?: "); 
+        /*fazer enquanto a condição do while não for satisfeita*/
         scanf (" %c", &times);
-        toupper (times);    
         
-        if ((times != 'A' && times != 'C' && times != 'F' && times != 'S' ) 
-        printf ("Digite uma das resposta disponiveis!");
+        times = toupper (times); /*transformar tudo em maiúsculo*/
         
-    } while (times != 'A' && times != 'C' && times != 'F' && times != 'S');
+        if ((times != 'A' && times != 'C' && times != 'F' && times != 'S' ))
+        /*se o user nao colocar nenhuma das disponiveis mostrar o printf abaixo*/
+        printf ("Digite uma das resposta disponiveis!\n");
+        
+        if (times == 'S') {
+        /*se o usuario digitar s, sai do programa*/
+        printf ("\nSaindo do programa.\n"); 
+       
+        } else if (times == 'A'){
+        /*quando digita o a, o atletico que era 0 aumenta 1, e a mesma coisa pros outros*/
+        atle++;
+        
+        } else if (times == 'C'){
+        coxa++;
+        
+        } else if (times == 'F'){
+        foz++;
+            
+        }
     
+        total = (foz + coxa + atle);
+     
+        
+    } while (times != 'S');
     
+    printf ("O total de torcedores do Atlético é: %d de %d\n", atle, total);
+    printf ("O total de torcedores do Coritiba é: %d de %d\n", coxa, total);
+    printf ("O total de torcedores do Foz é: %d de %d\n", foz, total);
+    
+    printf("Porcentagem Atletico: %.2f%%\n", (float)atle / total * 100);
+    printf("Porcentagem Coritiba: %.2f%%\n", (float)coxa / total * 100);
+    printf("Porcentagem Foz: %.2f%%\n", (float)foz / total * 100);
+
+    /*usando duas % para poder mostrar uma pro usuario
+    .2f para 2 casas apos a virgula
+    utilizando o (cast) para dar certo a divisao
+    multiplicando por cem para aparecer "inteiro" para o usuario, e não um numero pequeno
+    os printf's estao fora do loop pq se não toda vez que respondesse, ia mostrar as mensagens
+    
+    ignora professora, uso os comentarios para nn me perder e também para estudar*/
+   
+    return 0;
 }
