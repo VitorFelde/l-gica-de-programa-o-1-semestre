1287e971d7dc3087568888f4133f1afaf4e50af2
Vitor Felde <feldevitor@gmail.com>
2026-05-20 09:02:26 -0300

    Improve string reversal implementation
    Refactor string reversal logic and fix output format.

--- lista 11 - string/atividade6.c
+++ lista 11 - string/atividade6.c
@@ -5,21 +5,28 @@ Saída: etset*/
 
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 int main () {
 	
-	int i, stringInv[50];
-	char string1[50];
+	int i, tamanhotop;
+	char stringInv[50], string1[50], j = 0;
 	
 	printf ("Digite sua sentença: ");
 	fgets (string1, sizeof(string1), stdin);
 	
+	tamanhotop = strlen (string1);//obtendo o tamanho da string
 	
-	for (i = 0; i < 50; i++) {
-			stringInv[i] = string1[i] - 1 - i;  
+	for (i = tamanhotop - 1; i >= 0; i--) { //tamanhotop é a variavel criada para obter o tamanho
+	//da string que o usuario digitou inicialmente, dai fazemos menos 1, pois o ultimo digito da string
+	//é o \0, que é nulo
+	    stringInv[j] = string1[i]; //contador do i, diminui, e do j cresce, fazendo a op de inversao
+	    j++;
 	}
-	printf ("Sua sentença inversa é: %c", stringInv);
+	
+	stringInv[j] = '\0'; //para determinar o fim da nova string
+	
+	printf ("Sua sentença inversa é: %s", stringInv);
+
 	
 	return 0;
 	
