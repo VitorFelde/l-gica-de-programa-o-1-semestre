d37fce9f9aa114f41c58e4de4bfa51bacdc255b2
Vitor Felde <feldevitor@gmail.com>
2026-05-20 09:38:19 -0300

    Update atividade6.c

--- lista 10 - matriz/atividade6.c
+++ lista 10 - matriz/atividade6.c
@@ -1,25 +1,46 @@
-/*6) Faça um programa para calcular e apresentar a média geral de uma turma de n alunos. A
-média a ser obtida deve ser a média geral de cada aluno durante o ano letivo das quatro notas.
-Apresentar o resultado da forma abaixo:
-ALUNO NOTA1 NOTA2 NOTA3 NOTA4 MEDIA
-1 8.0 9.0 8.0 7.0 8.0
-2 9.0 7.0 6.0 7.0 7.2
-3 5.0 6.0 7.0 6.0 6.0
-MEDIA GERAL.......................: 7.1*/
-
 #include <stdio.h>
 
 int main()
 {
-    
-    float media = 0, nota, soma = 0;
     int n;
     
-    printf ("Qual o número de alunos na sala?: ");
-    scanf ("%d", &n);
+    printf("Qual o número de alunos na sala?: ");
+    scanf("%d", &n);
     
-    int matriz[n][4];
-    //aqui colocamos 4, pois as colunas serao as notas, e sao 4 notas + media, portanto nao é necessario mais, so as linhas mudam
+   
+    float notas[n][4]; //4 fixo pois sao 4 colunas de qqlr jeito, nao importa o numero de alunos
+    //vetorzao para armazenar a média de cada aluno
+    float mediasalunos[n];
+    float somageral = 0.0;
+    float mediageral = 0.0;
+    
+    for (int i = 0; i < n; i++) { //dentro do for, pois vai pedir enquanto tiver aluno
+        float somaaluno = 0.0; //nn sei pq cria aqui, mas foi a solucao que acehi
+        printf("\nDigite as 4 notas do Aluno %d:", i + 1); //colocamos + 1 pois comeca em 0
+        
+        for (int j = 0; j < 4; j++) {
+            printf("Nota %d: ", j + 1); //mesma coisa la de cima, +1 pq cmc em 0
+            scanf("%f", &notas[i][j]);
+            somaaluno = somaaluno + notas[i][j];
+        }
+        
+        
+        mediasalunos[i] = somaaluno / 4.0; //calcula media do aluno
+
+        somageral = somageral + mediasalunos[i]; //soma com a media da trm q comeca em 0, com a do aluno
+    }
+    
+    mediageral = somageral / n;
+    
+    printf("\nALUNO\tNOTA1\tNOTA2\tNOTA3\tNOTA4\tMEDIA\n"); //t para tabulacao, que é o espaco que fica enre os elementos
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n", i + 1, notas[i][0], notas[i][1], notas[i][2], notas[i][3], mediasalunos[i]);
+        //i + 1 pois comeca em 0, e dai imprimos as notas dos alunos, com o i, que cresce de acordo com o numero total de aluno, mas 
+        //a coluna fica fixa, entao por isso coloca valores fixos e nao crescentes
+    }//t para ficar bonitinho - tive que pesquisar como deixar formatado assim
+    
+    printf("MEDIA GERAL: %.2f\n", mediageral); //nao precisava deixar aqueles pontinhos ne professora?!
     
     return 0;
 }
