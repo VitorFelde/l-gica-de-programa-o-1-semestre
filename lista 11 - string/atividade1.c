9281913f05f808e6690a36aba8ca8425684712a9
Vitor Felde <feldevitor@gmail.com>
2026-05-19 21:09:36 -0300

    Allow user input for string concatenation
    Modified the program to accept user input for two strings instead of using predefined values. Added logic to remove the newline character from the input.

--- lista 11 - string/atividade1.c
+++ lista 11 - string/atividade1.c
@@ -8,7 +8,16 @@ Após concatenar: string1: luacheia string2: cheia*/
 
 int main () {
 	
-	char string1[50] = "lua", string2[50] = "cheia";
+	
+	char string1[50], string2[50];
+	
+	printf ("Digite a primeira string: ");
+	fgets (string1, sizeof(string1), stdin);
+	
+	printf ("Digite a primeira string: ");
+	fgets (string2, sizeof(string2), stdin);
+	
+	string1[strlen(string1) - 1] = '\0'; //a string 1 pega o tamanho dela mesma, diminui 1, para pegar o \n automatico e atribui \0
 	
 	strcat (string1, string2);
 	
